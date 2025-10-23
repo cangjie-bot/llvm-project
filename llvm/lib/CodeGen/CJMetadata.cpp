184e552f2ccdbbb2909c6bc5d6a0f110b534cdae
wzy_00889928 <1322947566@qq.com>
2025-10-23 21:54:36 +0800

    feat: 930 opensource
    Signed-off-by: wzy_00889928 <1322947566@qq.com>

--- llvm/lib/CodeGen/CJMetadata.cpp
+++ llvm/lib/CodeGen/CJMetadata.cpp
@@ -442,11 +442,17 @@ void CJMetadataInfo::emitStackTraceInfo(const MCSymbol *FuncSym,
   OS.emitValue(getOrInsertStrPoolOffset(MethodNameStrIndex, DescSym), StrSize);
   OS.emitValue(getOrInsertStrPoolOffset(DirStrIndex, DescSym), StrSize);
   OS.emitValue(getOrInsertStrPoolOffset(FileNameStrIndex, DescSym), StrSize);
-  OS.emitValue(
-      MCBinaryExpr::createSub(
-          MCSymbolRefExpr::create(StrPoolDictOffsetsSym, AP.OutContext),
-          MCSymbolRefExpr::create(DescSym, AP.OutContext), AP.OutContext),
-      StrSize);
+
+  if (MethodCompressedCode.empty() && DirCompressedCode.empty() &&
+      FileCompressedCode.empty()) {
+    OS.emitIntValue(0, 4);
+  } else {
+    OS.emitValue(
+        MCBinaryExpr::createSub(
+            MCSymbolRefExpr::create(StrPoolDictOffsetsSym, AP.OutContext),
+            MCSymbolRefExpr::create(DescSym, AP.OutContext), AP.OutContext),
+        4);
+  }
   return;
 }
 
@@ -705,6 +711,9 @@ void CJMetadataInfo::emitGlobalInitFuncTable() {
     return;
 
   OS.switchSection(TD[GlobalInitFuncIdx].TableSection);
+  if (TT.isOSBinFormatMachO())
+    // 8: align size, 8 bytes
+    OS.emitValueToAlignment(8);
   NamedMDNode *PkgInitFuncMD = M->getNamedMetadata("pkg_init_func");
   std::string GlobalInitFuncName;
   if (PkgInitFuncMD != nullptr) {
