0afaf5dc34f74017b851538709241155cb4dc529
wanghuihui <wanghuihui17@huawei.com>
2025-11-08 17:45:26 +0800

    feat: fix stacksize
    Signed-off-by: wanghuihui <wanghuihui17@huawei.com>

--- llvm/lib/CodeGen/StackMaps.cpp
+++ llvm/lib/CodeGen/StackMaps.cpp
@@ -473,8 +473,8 @@ StackMaps::parseStructArgsOperand(MachineInstr::const_mop_iterator MOI,
     assert(DL.getStructLayout(ST)->getSizeInBytes() > Offset &&
            "The offset cannot be greater than the size of struct.");
 #endif
-    // 8:ptr size
-    FOLocations.emplace_back(StackMaps::Location::Indirect, 8,
+    // 4/8:ptr size
+    FOLocations.emplace_back(StackMaps::Location::Indirect, FuncPtrSize,
                              getDwarfRegNum(Reg, TRI), Imm + Offset);
   } else if (MOI->getImm() == StackMaps::IndirectMemRefOp) {
     int64_t Size = (++MOI)->getImm();
@@ -511,16 +511,16 @@ StackMaps::parseAllocaOperand(MachineInstr::const_mop_iterator MOI,
     } else if (auto *PT = dyn_cast<PointerType>(Inst->getAllocatedType())) {
       if (*GS->isGCManagedPointer(PT) ||
           *GS->isGCManagedPointer(PT->getElementType())) {
-        // 8:ptr size
-        Locations.emplace_back(StackMaps::Location::Indirect, 8,
+        // 4/8:ptr size
+        Locations.emplace_back(StackMaps::Location::Indirect, FuncPtrSize,
                                getDwarfRegNum(Reg, TRI), Imm);
       }
     }
   } else {
     // The FI of alloca may be modify in tryToElideArgumentCopy.
     // In this case, alloca type must be a pointer type.
-    // 8:ptr size
-    Locations.emplace_back(StackMaps::Location::Indirect, 8,
+    // 4/8:ptr size
+    Locations.emplace_back(StackMaps::Location::Indirect, FuncPtrSize,
                            getDwarfRegNum(Reg, TRI), Imm);
   }
   return ++MOI;
@@ -544,8 +544,8 @@ StackMaps::parseStackPtrOperand(MachineInstr::const_mop_iterator MOI,
     int64_t FI = (++MOI)->getImm();
     (void)FI;
     assert((Imm % OffsetStepSize == 0) && "Stack Offset align error!");
-    Locations.emplace_back(StackMaps::Location::Indirect, 8,
-                           getDwarfRegNum(Reg, TRI), Imm); // 8: pointer size
+    Locations.emplace_back(StackMaps::Location::Indirect, FuncPtrSize,
+                           getDwarfRegNum(Reg, TRI), Imm); // 4/8: pointer size
   } else {
     report_fatal_error("MOI is error when parseStackPtrOperand!");
   }
@@ -749,6 +749,9 @@ void StackMaps::updateOrInsertFnInfo(const MCSymbol *FnSym,
     if (CJPipeline && AP.getSubtargetInfo().getTargetTriple().isOSWindows()) {
       FrameSize = MFI.getWin64FramePointerOffset();
     }
+    if (CJPipeline && isARM()) {
+      FrameSize = MFI.getStackSize();
+    }
     // get func callee saved regInfo
     const auto &CSInfo = AP.MF->getFrameInfo().getCalleeSavedInfo();
     std::map<unsigned, int> CSReg2Stack; // <reg, stackOffset>
@@ -758,6 +761,8 @@ void StackMaps::updateOrInsertFnInfo(const MCSymbol *FnSym,
         continue;
       }
       unsigned RegNo = getDwarfRegNum(CS.getReg(), RegInfo);
+      if (isARM() && (RegNo == 11 /*SP*/ || RegNo == 14 /*LR*/))
+        continue;
       Register Reg;
       CSReg2Stack[RegNo] =
           TFI->getFrameIndexRefForCJ(*AP.MF, CS.getFrameIdx(), Reg).getFixed();
@@ -1829,7 +1834,7 @@ void DataEncoder::emitCommentForSlots(raw_svector_ostream &Comment,
     while (SlotBitBitNums > 64) {
       SlotBitBitNums -= 64;
       EmitSlotBitComment(readBits(64), 64);
-      BaseOffset -= 64 * 8; // each bit represents 8 bytes
+      BaseOffset -= 64 * FuncPtrSize; // each bit represents 4/8 bytes
     }
     EmitSlotBitComment(readBits(SlotBitBitNums), SlotBitBitNums);
   } else {
