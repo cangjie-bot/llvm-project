51ddc8af8289c68d41cf9afcfad0eef194571b67
kangyi <kangyi9@h-partners.com>
2025-11-15 17:32:43 +0800

    feat: compatibility adaptation
    Signed-off-by: kangyi <kangyi9@h-partners.com>

--- llvm/lib/CodeGen/CJMetadata.cpp
+++ llvm/lib/CodeGen/CJMetadata.cpp
@@ -92,6 +92,7 @@ void CJMetadataInfo::init() {
   TD[TypeInfoTableIdx].TableSection = MOFI->getTypeInfoSection();
   TD[TypeTemplateIdx].TableSection = MOFI->getCJTypeTemplateSection();
   TD[TypeFieldsIdx].TableSection = MOFI->getCJTypeFieldsSection();
+  TD[TypeExtTableIdx].TableSection = MOFI->getTypeExtSection();
   TD[MTableIdx].TableSection = MOFI->getCJMtableSection();
   TD[StaticGIIdx].TableSection = MOFI->getCJStaticGenericTISection();
   TD[GCFlagsIdx].TableSection = MOFI->getCJGCFlagsSection();
@@ -157,6 +158,11 @@ void CJMetadataInfo::recordGlobalVariable(const GlobalVariable *GV) {
 
     if (GV->isCJMTable()) {
       MTableTable.push_back(GV);
+      return;
+    }
+
+    if (GV->isCJTypeExt()) {
+      TypeExtTable.push_back(GV);
       return;
     }
 
@@ -613,6 +619,13 @@ void CJMetadataInfo::emitTypeTemplate() {
     AP.emitGlobalVariable(TT);
 }
 
+void CJMetadataInfo::emitTypeExt() {
+  OS.switchSection(TD[TypeExtTableIdx].TableSection);
+  for (const auto *typeExt : TypeExtTable) {
+    AP.emitGlobalVariable(typeExt);
+  }
+}
+
 void CJMetadataInfo::emitMTable() {
   if (MTableTable.empty())
     return;
@@ -869,6 +882,7 @@ void CJMetadataInfo::emitCJMetadata(Module &M) {
   emitGCFlags();
   emitReflectInfo();
   emitReflectGenericTI();
+  emitTypeExt();
   OS.addBlankLine();
 }
 } // namespace llvm
