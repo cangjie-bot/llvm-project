f8707a196c7b60cbd69c6216a93b5fb75e6bb5f5
wanghuihui <wanghuihui17@huawei.com>
2025-12-10 16:13:02 +0800

    fix: compile time optimization
    Signed-off-by: wanghuihui <wanghuihui17@huawei.com>

--- llvm/include/llvm/Analysis/InlineCost.h
+++ llvm/include/llvm/Analysis/InlineCost.h
@@ -246,8 +246,6 @@ InlineParams getInlineParams(unsigned OptLevel, unsigned SizeOptLevel);
 /// and the call/return instruction.
 int getCallsiteCost(CallBase &Call, const DataLayout &DL);
 
-bool checkCJStackMapThreshold(CallBase &Call, Function &F);
-
 /// Get an InlineCost object representing the cost of inlining this
 /// callsite.
 ///
