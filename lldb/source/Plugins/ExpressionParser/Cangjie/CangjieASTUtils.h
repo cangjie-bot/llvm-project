e2a4ac442e35605dc7c3b6849213230e6690ec11
yuanchao <yuanchao55@h-partners.com>
2026-01-12 11:51:50 +0800

    fix: bugfix gengric expression
    Signed-off-by: yuanchao <yuanchao55@h-partners.com>

--- lldb/source/Plugins/ExpressionParser/Cangjie/CangjieASTUtils.h
+++ lldb/source/Plugins/ExpressionParser/Cangjie/CangjieASTUtils.h
@@ -77,6 +77,7 @@ bool StartsWith(const std::string& str, const std::string& pre);
 std::string GetPkgNameFromCompilerUnit(const CompileUnit& compUnit);
 std::string GetSubNameWithoutPkgname(std::string& name, std::string& pkg);
 std::string DeleteAllPkgname(const std::string& name, std::string& pkg);
+std::string lldb_private::DeleteAllStdPkgname(const std::string& name);
 std::string DeletePrefixOfType(const std::string& name);
 bool IsGenericTypeParameters(const std::string& type_name);
 bool IsGenericType(const std::string& type_name);
