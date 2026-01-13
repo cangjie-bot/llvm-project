7c5f6c05680ec165bdd8cb78355f1686867fd201
lijiawei_123 <lixpoo@163.com>
2026-01-13 20:02:17 +0800

    feat: test
    Signed-off-by: lijiawei_123 <lixpoo@163.com>

--- llvm/lib/Transforms/IPO/GlobalDCE.cpp
+++ llvm/lib/Transforms/IPO/GlobalDCE.cpp
@@ -1,4 +1,4 @@
-//===-- GlobalDCE.cpp - DCE unreachable internal functions ----------------===//
+//123===-- GlobalDCE.cpp - DCE unreachable internal functions ----------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
