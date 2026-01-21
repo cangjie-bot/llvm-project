33f110c67f3f02841f43720f4c7d8adafa1caf5b
linyudong <linyudong4@huawei.com>
2026-01-21 14:31:17 +0800

    feat: replace_GC_instr_with_ld_store_instr
    Signed-off-by: linyudong <linyudong4@huawei.com>

---
+++ llvm/include/llvm/Transforms/Scalar/CJGCInstrReplace.h
@@ -0,0 +1,43 @@
+//===- CJGCInstrReplace.h - -----------------------------------------*- C++
+//-*-===//
+//
+// Copyright (c) Huawei Technologies Co., Ltd. 2026. All rights reserved.
+// This source file is part of the Cangjie project, licensed under Apache-2.0
+// with Runtime Library Exception.
+//
+// See https://cangjie-lang.cn/pages/LICENSE for license information.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file provides interface to "Cangjie GC Instruction Replace" pass.
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_TRANSFORMS_CJ_GC_INSTR_REPLACE_H
+#define LLVM_TRANSFORMS_CJ_GC_INSTR_REPLACE_H
+
+#include "llvm/ADT/SmallVector.h"
+#include "llvm/IR/InstrTypes.h"
+#include "llvm/IR/PassManager.h"
+
+namespace llvm {
+
+class CJGCInstrReplace : public PassInfoMixin<CJGCInstrReplace> {
+public:
+  CJGCInstrReplace() = default;
+  PreservedAnalyses run(Function &F, FunctionAnalysisManager &AM);
+  static constexpr StringRef GC_WRITE_REF_IID = "GC_Write_Ref_IID";
+
+private:
+  bool runImpl(Function &F);
+  void collectGCInstr(Function &F);
+  void dispatchGCInstr(CallBase *CB);
+  bool changeGCInstrToLSInstr();
+  void initContainer();
+  SmallVector<CallBase *, 4> GCReadRefs;
+  SmallVector<CallBase *, 4> GCWriteRefs;
+  SmallDenseMap<Intrinsic::ID, SmallVector<CallBase *, 4> *> GCInstrDispatchMap;
+};
+
+} // end namespace llvm
+
+#endif // LLVM_TRANSFORMS_CJ_GC_INSTR_REPLACE_H
