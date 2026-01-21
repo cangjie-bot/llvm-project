ed7cd9dcb0e089ab44342e82d2a714e20ecacccf
leeguo <liguo21@huawei.com>
2026-01-21 17:21:34 +0800

    fix: parse modify to reflect version
    Signed-off-by: leeguo <liguo21@huawei.com>

--- llvm/include/llvm/Transforms/Scalar/CJFillMetadata.h
+++ llvm/include/llvm/Transforms/Scalar/CJFillMetadata.h
@@ -144,7 +144,10 @@ enum ReflectModifyType : uint32_t {
   RMT_HAS_SRET2 = 0x1 << 19, // has sret and it is 'known struct T'
   RMT_HAS_SRET3 = 0x1 << 20, // has sret and it is 'unknow struct T'
   RMT_ENUM_CTOR = 0x1 << 21,
-  RMT_MAX = 0x1 << 22,
+  RMT_REFLECT_VER_BIT1 = 0,
+  RMT_REFLECT_VER_BIT2 = 0,
+  RMT_REFLECT_VER_BIT3 = 0,
+  RMT_MAX = 0x1U << 31,
 };
 
 struct ExtensionDefData {
