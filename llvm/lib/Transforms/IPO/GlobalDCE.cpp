872b8d096cd86481e420d6a9bc79d6c48a54c02a
lijiawei_123 <lixpoo@163.com>
2026-03-12 21:16:31 +0800

    fix: error GFI update in VFE
    Signed-off-by: lijiawei_123 <lixpoo@163.com>

--- llvm/lib/Transforms/IPO/GlobalDCE.cpp
+++ llvm/lib/Transforms/IPO/GlobalDCE.cpp
@@ -515,6 +515,9 @@ CangjieDCE::insertInstance(const SmallVectorImpl<GlobalVariable *> &Path,
       continue;
     }
     auto *GFI = new GenericFuncInfo;
+#ifndef NDEBUG
+    GFI.Ty = GV;
+#endif
     GFI->Prev = CurRoot;
     CurRoot->Next[GV] = GFI;
     CurRoot = GFI;
@@ -623,8 +626,9 @@ void CangjieDCE::updateDependencies(
   };
   for (auto &[GFI, Offsets] : Relation) {
     for (auto I : Offsets) {
+      auto *CurGFI = GFI;
       // 1. Mark all child nodes.
-      SmallVector<GenericFuncInfo *, 8> Worklist = {GFI};
+      SmallVector<GenericFuncInfo *, 8> Worklist = {CurGFI};
       while (!Worklist.empty()) {
         auto *Child = Worklist.pop_back_val();
         for (auto [_, N] : Child->Next) {
@@ -633,9 +637,9 @@ void CangjieDCE::updateDependencies(
         }
       }
       // 2. Recursively mark all parent nodes.
-      while (GFI != Root) {
-        auto &FTs = GFI->FuncTables;
-        GFI = GFI->Prev;
+      while (CurGFI != Root) {
+        auto &FTs = CurGFI->FuncTables;
+        CurGFI = CurGFI->Prev;
         Mark(FTs, I);
       }
     }
