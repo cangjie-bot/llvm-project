24f5efdff25d44a898534372e2ded919c288c28a
zichexuelan <sundaiyue@foxmail.com>
2026-08-28 11:52:36 +0800

    chore: update version to 1.3.0-alpha.03
    Co-authored-by: zichexuelan<sundaiyue@foxmail.com>

--- lldb/source/Version/Version.cpp
+++ lldb/source/Version/Version.cpp
@@ -58,7 +58,7 @@ const char *lldb_private::GetVersion() {
       g_version_str += "\n  llvm revision ";
       g_version_str += llvm_rev;
     }
-    g_version_str += "\n  cangjie 1.3.0-alpha.02";
+    g_version_str += "\n  cangjie 1.3.0-alpha.03";
   }
 
   return g_version_str.c_str();
