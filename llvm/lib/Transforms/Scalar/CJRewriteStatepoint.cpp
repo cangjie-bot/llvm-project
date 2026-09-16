e3ec0240ba51fb4c16df293890c79fa2c0af240c
yuqiaoyu <yujunqiang1@h-partners.com>
2026-09-16 18:11:08 +0800

    fix: revert miss SExt\ZExt\NoUndef attrs of arguments after RewriteStatepoin
    This reverts commit b7c2d29e3763f3ece28d6efb05fc4a7cd497ee7a.
    
    Signed-off-by: yuqiaoyu <yujunqiang1@h-partners.com>

--- llvm/lib/Transforms/Scalar/CJRewriteStatepoint.cpp
+++ llvm/lib/Transforms/Scalar/CJRewriteStatepoint.cpp
@@ -1393,9 +1393,8 @@ static AttributeList legalizeCallAttributes(CallBase *Call,
   // invalid after lowering are stripped in stripNonValidDataFromBody.
   for (unsigned I : llvm::seq<unsigned>(0, Call->arg_size())) {
     AttributeSet AS = OrigAL.getParamAttrs(I);
-    if (AS.hasAttribute(Attribute::ByVal) || AS.hasAttribute(Attribute::StructRet) ||
-        AS.hasAttribute(Attribute::SExt) || AS.hasAttribute(Attribute::ZExt) ||
-        AS.hasAttribute(Attribute::NoUndef)) {
+    if (AS.hasAttribute(Attribute::ByVal) ||
+        AS.hasAttribute(Attribute::StructRet)) {
       StatepointAL = StatepointAL.addParamAttributes(
           Ctx, GCStatepointInst::CallArgsBeginPos + I, AttrBuilder(Ctx, AS));
     }
